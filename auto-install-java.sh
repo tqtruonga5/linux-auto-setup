15ed561eb68cdc29688b52f933c5fab5a2661fa1
Truong Tran <truongtran.cs@gmail.com>
2017-05-09 21:18:00 +0700

    wipe java auto install script

--- auto-install-java.sh
+++ auto-install-java.sh
@@ -4,22 +4,3 @@ sudo apt-get update
 sudo apt-get install -y oracle-java8-installer
 echo "--------\n"
 java -version
-
-# nodejs npm
-sudo apt-get install -y python-software-properties
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# config npm with root 
-cd ~
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-
-#bash shell
-echo '\nexport PATH=~/.npm-global/bin:$PATH' >> ~/.profile
-source ~/.profile
-
-#zshell
-echo '\nexport PATH=~/.npm-global/bin:$PATH' >> ~/.zshrc
-source ~/.zshrc
-# -----------------------
