1e0ce082c8462e1dd783ce0f557d7d26d339c497
Truong Tran <truongtran.cs@gmail.com>
2017-05-09 21:22:05 +0700

    separate npm-auto-install from java-auto-install file

---
+++ npm-scripts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# nodejs npm
+sudo apt-get install -y python-software-properties
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# config npm with root 
+cd ~
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+
+#bash shell
+echo '\nexport PATH=~/.npm-global/bin:$PATH' >> ~/.profile
+source ~/.profile
+
+#zshell
+echo '\nexport PATH=~/.npm-global/bin:$PATH' >> ~/.zshrc
+source ~/.zshrc
+# -----------------------
